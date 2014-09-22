0f97bdf8450f499daf0d52bd8c38884e80b2c24b
Sekerak <sekys.sekys@gmail.com>
2014-09-22 18:17:26 +0200

    Pridany servlet a konfiguracna trieda.

---
+++ src/service/Servlet.java
@@ -0,0 +1,31 @@
+package service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+/**
+ * Created by Seky on 17. 7. 2014.
+ * <p/>
+ * Servlet pre TImeline.
+ */
+public class Servlet extends HttpServlet {
+    private static final Logger LOGGER = Logger.getLogger(Servlet.class.getName());
+    private static final int CACHE_DURATION_IN_SECOND = 0; // 60 * 60 * 24 * 2; // 2 days
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String nameA = req.getParameter("first");
+        String nameB = req.getParameter("second");
+        resp.setContentType(MediaType.APPLICATION_JSON);
+    }
+
+}
