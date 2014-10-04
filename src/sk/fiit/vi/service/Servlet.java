37e15ca557f6a1591f383503f3aa662ca9cd87ef
Sekerak <sekys.sekys@gmail.com>
2014-10-04 20:20:32 +0200

    Dopisany format vystupu pre osoby.

--- src/sk/fiit/vi/service/Servlet.java
+++ src/sk/fiit/vi/service/Servlet.java
@@ -21,6 +21,13 @@ public class Servlet extends HttpServlet {
     private static final Logger LOGGER = Logger.getLogger(Servlet.class.getName());
     private static final ObjectMapper MAPPER = new ObjectMapper();
 
+    private void printPeople(PrintWriter writer, String title, List<Person> people) {
+        writer.println("</br>");
+        writer.println(title + "</br>");
+        for (Person p : people) {
+            writer.println(p.toString() + "</br>");
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -29,16 +36,20 @@ public class Servlet extends HttpServlet {
 
         resp.setHeader("Content-Type", "text/html");
         PrintWriter writer = resp.getWriter();
-        writer.println("<html><body><pre>Vysledok</br>");
+        writer.println("<html><body><pre>");
 
         try {
             List<Person> peopleA = Lucene.getInstance().find(nameA);
             List<Person> peopleB = Lucene.getInstance().find(nameB);
 
+            printPeople(writer, "People A", peopleA);
+            printPeople(writer, "People B", peopleB);
+            writer.println("</br>");
+            writer.println("Porovnanie</br>");
             for (Person a : peopleA) {
                 for (Person b : peopleB) {
                     Boolean intersect = a.checkIntersection(b);
-                    writer.printf("%s - %s = %s</br>", a.getNames().get(0), b.getNames().get(0));
+                    writer.printf("%s - %s = %s</br>", a.getNames().get(0), b.getNames().get(0), intersect == null ? "?" : intersect.toString());
                 }
             }
 
